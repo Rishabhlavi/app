77aa31c7da45dfcbc9f273c3b6bb457b4c3c5d80
gcarlo.lelli@live.com <@1github1@>
2015-03-12 16:19:31 +0100

    new website

---
+++ Xamarin.Data/Models/University.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+#if ASPNET
+    using System.ComponentModel.DataAnnotations;
+#endif
+
+namespace Xamarin.Data.Models
+{
+    public class University
+    {
+        public University()
+        {
+            Ambassadors = new HashSet<Ambassador>();
+        }
+
+        [DataMember]
+#if ASPNET
+        [Display(Name="University name"), ScaffoldColumn(false)]
+#endif
+        public int Id { get; set; }
+
+        [DataMember]
+#if ASPNET
+        [Display(Name = "Name"), Required]
+#endif
+        public String Name { get; set; }
+
+        [DataMember]
+#if ASPNET
+        [Display(Name = "Website"), Required, DataType(DataType.Url)]
+#endif
+        public String WebSite { get; set; }
+
+        [DataMember]
+#if ASPNET
+        [Display(Name = "Email"), DataType(DataType.EmailAddress)]
+#endif
+        public String ContactEmail { get; set; }
+
+        [DataMember]
+#if ASPNET
+        [Display(Name = "Logo"), DataType(DataType.Upload)]
+#endif
+        public String Logo { get; set; }
+
+        [IgnoreDataMember]
+#if ASPNET
+        [ScaffoldColumn(false)]
+#endif
+        public virtual ICollection<Ambassador> Ambassadors { get; set; }
+    }
+}
