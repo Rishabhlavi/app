57dbf1487238e8b4648e177d670f7d3b51315190
gcarlo.lelli@live.com <@1github1@>
2015-04-11 15:53:32 +0200

    added api help page

---
+++ Xamarin.Data/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Xamarin.Data.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
